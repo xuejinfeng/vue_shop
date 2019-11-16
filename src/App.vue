adcb931114d799d12686e2baab6457b05e4daad6
xuejinfeng <1037298133@qq.com>
2019-11-16 12:01:33 +0800

    vue项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- 路由占位符 -->
+    //login和主页
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+</style>
---
+++ src/router.js
@@ -0,0 +1,62 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+import Welcome from './components/Welcome.vue'
+import Users from './components/user/Users.vue'
+import Rights from './components/power/Rights.vue'
+import Roles from './components/power/Roles.vue'
+import Cate from './components/goods/Cate.vue'
+import Params from './components/goods/Params.vue'
+import GoodsList from './components/goods/List.vue'
+import Add from './components/goods/Add.vue'
+import Order from './components/order/Order.vue'
+import Report from './components/report/Report.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users },
+        { path: '/rights', component: Rights },
+        { path: '/roles', component: Roles },
+        { path: '/categories', component: Cate },
+        { path: '/params', component: Params },
+        { path: '/goods', component: GoodsList },
+        { path: '/goods/add', component: Add },
+        { path: '/orders', component: Order },
+        { path: '/reports', component: Report }
+      ]
+    }
+  ]
+})
+//通过路由导航控制访问权限，
+//如果用户没有登录，但是直接通过url访问特定的页面，需要重新导航到登录页面
+//其实我们也可以通过普通判断token的方式，再加上push方法也可以实现，但是不科学
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //     next()  放行    next('/login')  强制跳转
+
+  //登录直接放行
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  //没有token强制跳转到登录页
+  if (!tokenStr) return next('/login')
+    //如果token存在直接放行
+  next()
+})
+
+export default router
